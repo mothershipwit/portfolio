c91398acd7270c103c00079d34d6a752b138875c
Witni <witni.whittle@icloud.com>
2020-09-29 13:02:00 -0500

    added style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,21 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+
 
 export default function About() {
   return (
       <Layout>
+        
             <div><h1>About me</h1></div>
             <p> My name is Witni and I am a mom of 4 that loves food.
                 I love to support local Restaurants, as well as cooking
                 my families scratch made meals.
             </p>
-            <p>Follow my Facebook Group, 
+            <p>Follow my Facebook Group,  <a href="https://www.facebook.com/groups/nwafoodjunkies">NWA Food Junkies</a> if you like to follow local establishments. 
    
             </p>
       </Layout>
   )
 }
+
+
